3b4701dd679d52146fb8819a4f2580733d36913f
Denis <denis.tojaga@edu.fit.ba>
2020-12-18 08:16:40 +0100

    //Logo added

--- frmLogin.cs
+++ frmLogin.cs
@@ -12,9 +12,41 @@ namespace Hospital_System_Demo
 {
     public partial class frmLogin : Form
     {
+        private PictureBox pictureBox1;
+
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Hospital_System_Demo.Properties.Resources.hospitalLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(325, 468);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.Text = "Login to Health Care";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
     }
 }
