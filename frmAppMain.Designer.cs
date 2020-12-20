1dd5725f76df6f3bd5e7e745167b451ec828402a
Denis <denis.tojaga@edu.fit.ba>
2020-12-20 18:39:13 +0100

    //Main form added

--- frmAppMain.Designer.cs
+++ frmAppMain.Designer.cs
@@ -28,71 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tcHome = new System.Windows.Forms.TabControl();
-            this.tPHome = new System.Windows.Forms.TabPage();
-            this.tpNext = new System.Windows.Forms.TabPage();
-            this.tcHome.SuspendLayout();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panelTitle.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tcHome
+            // panelTitle
             // 
-            this.tcHome.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.panelTitle.Controls.Add(this.btnClose);
+            this.panelTitle.Controls.Add(this.lblTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1076, 106);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.lblTitle.Location = new System.Drawing.Point(200, 21);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(642, 67);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HEALTH CARE APP";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tcHome.Appearance = System.Windows.Forms.TabAppearance.Buttons;
-            this.tcHome.Controls.Add(this.tPHome);
-            this.tcHome.Controls.Add(this.tpNext);
-            this.tcHome.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.tcHome.Font = new System.Drawing.Font("Segoe UI Semibold", 22.75F, System.Drawing.FontStyle.Bold);
-            this.tcHome.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.tcHome.Location = new System.Drawing.Point(12, 39);
-            this.tcHome.Name = "tcHome";
-            this.tcHome.SelectedIndex = 0;
-            this.tcHome.Size = new System.Drawing.Size(1070, 530);
-            this.tcHome.TabIndex = 0;
-            this.tcHome.Tag = "";
-            // 
-            // tPHome
-            // 
-            this.tPHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
-            this.tPHome.Font = new System.Drawing.Font("Segoe UI Semibold", 22.75F, System.Drawing.FontStyle.Bold);
-            this.tPHome.Location = new System.Drawing.Point(4, 53);
-            this.tPHome.Name = "tPHome";
-            this.tPHome.Padding = new System.Windows.Forms.Padding(3);
-            this.tPHome.Size = new System.Drawing.Size(1062, 473);
-            this.tPHome.TabIndex = 0;
-            this.tPHome.Text = "Home";
-            // 
-            // tpNext
-            // 
-            this.tpNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
-            this.tpNext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tpNext.Location = new System.Drawing.Point(4, 53);
-            this.tpNext.Name = "tpNext";
-            this.tpNext.Padding = new System.Windows.Forms.Padding(3);
-            this.tpNext.Size = new System.Drawing.Size(1062, 473);
-            this.tpNext.TabIndex = 1;
-            this.tpNext.Text = "Next";
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClose.FlatAppearance.BorderSize = 2;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnClose.Location = new System.Drawing.Point(1023, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(41, 36);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
             // 
             // frmAppMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1094, 581);
-            this.Controls.Add(this.tcHome);
+            this.ClientSize = new System.Drawing.Size(1076, 606);
+            this.Controls.Add(this.panelTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmAppMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmAppMain";
-            this.tcHome.ResumeLayout(false);
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelTitle.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabControl tcHome;
-        private System.Windows.Forms.TabPage tPHome;
-        private System.Windows.Forms.TabPage tpNext;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnClose;
     }
 }
