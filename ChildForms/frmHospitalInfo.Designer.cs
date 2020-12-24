bd2f254b6a88567caee25c2509fe867417529d18
Denis <denis.tojaga@edu.fit.ba>
2020-12-24 17:35:17 +0100

    //Info edit

--- ChildForms/frmHospitalInfo.Designer.cs
+++ ChildForms/frmHospitalInfo.Designer.cs
@@ -36,6 +36,15 @@
             this.cmsOpenInMaps = new System.Windows.Forms.ToolStripMenuItem();
             this.lblLink = new System.Windows.Forms.LinkLabel();
             this.btnNavigate = new System.Windows.Forms.Button();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -44,17 +53,17 @@
             // 
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold);
             this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
-            this.lblTitle.Location = new System.Drawing.Point(576, 9);
+            this.lblTitle.Location = new System.Drawing.Point(785, 12);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(232, 50);
+            this.lblTitle.Size = new System.Drawing.Size(90, 50);
             this.lblTitle.TabIndex = 1;
-            this.lblTitle.Text = "Hospital Info";
+            this.lblTitle.Text = "Info";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.wbMaps);
-            this.panel1.Location = new System.Drawing.Point(37, 12);
+            this.panel1.Location = new System.Drawing.Point(47, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(442, 274);
             this.panel1.TabIndex = 2;
@@ -66,6 +75,7 @@
             this.wbMaps.Location = new System.Drawing.Point(0, 0);
             this.wbMaps.MinimumSize = new System.Drawing.Size(20, 20);
             this.wbMaps.Name = "wbMaps";
+            this.wbMaps.ScriptErrorsSuppressed = true;
             this.wbMaps.Size = new System.Drawing.Size(442, 274);
             this.wbMaps.TabIndex = 4;
             this.wbMaps.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.wbMaps_Navigated);
@@ -88,7 +98,7 @@
             this.lblLink.AutoSize = true;
             this.lblLink.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
             this.lblLink.LinkColor = System.Drawing.Color.White;
-            this.lblLink.Location = new System.Drawing.Point(485, 12);
+            this.lblLink.Location = new System.Drawing.Point(460, 12);
             this.lblLink.Name = "lblLink";
             this.lblLink.Size = new System.Drawing.Size(29, 13);
             this.lblLink.TabIndex = 4;
@@ -103,7 +113,7 @@
             this.btnNavigate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnNavigate.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold);
             this.btnNavigate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
-            this.btnNavigate.Location = new System.Drawing.Point(122, 292);
+            this.btnNavigate.Location = new System.Drawing.Point(132, 292);
             this.btnNavigate.Name = "btnNavigate";
             this.btnNavigate.Size = new System.Drawing.Size(244, 37);
             this.btnNavigate.TabIndex = 5;
@@ -113,6 +123,108 @@
             this.btnNavigate.MouseLeave += new System.EventHandler(this.btnNavigate_MouseLeave);
             this.btnNavigate.MouseHover += new System.EventHandler(this.btnNavigate_MouseHover);
             // 
+            // txtIme
+            // 
+            this.txtIme.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.txtIme.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtIme.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIme.ForeColor = System.Drawing.Color.White;
+            this.txtIme.Location = new System.Drawing.Point(638, 86);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(239, 26);
+            this.txtIme.TabIndex = 17;
+            this.txtIme.Text = "Dom Zdravlja Mostar";
+            this.txtIme.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGray;
+            this.label4.Location = new System.Drawing.Point(637, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 2);
+            this.label4.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(638, 190);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 36);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "124";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(638, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 2);
+            this.label1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(638, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 26);
+            this.textBox2.TabIndex = 21;
+            this.textBox2.Text = "domzdravlja@health.com";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(638, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 2);
+            this.label2.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 18.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.label3.Location = new System.Drawing.Point(549, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 35);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 18.25F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.label5.Location = new System.Drawing.Point(555, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 35);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 18.25F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(20)))));
+            this.label6.Location = new System.Drawing.Point(524, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 35);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Number";
+            // 
             // frmHospitalInfo
             // 
             this.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
@@ -120,6 +232,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(887, 556);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnNavigate);
             this.Controls.Add(this.lblLink);
             this.Controls.Add(this.panel1);
@@ -145,5 +266,14 @@
         private System.Windows.Forms.WebBrowser wbMaps;
         private System.Windows.Forms.LinkLabel lblLink;
         private System.Windows.Forms.Button btnNavigate;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
