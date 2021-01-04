2459c800a2704b08bb64e2a256b07c0c69c28357
Denis <denis.tojaga@edu.fit.ba>
2021-01-04 07:44:23 +0100

    //Landing page modification

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace Hospital_System_Demo.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap healthCareTeam {
+            get {
+                object obj = ResourceManager.GetObject("healthCareTeam", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hospitalLogo {
             get {
                 object obj = ResourceManager.GetObject("hospitalLogo", resourceCulture);
